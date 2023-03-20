1a969ec65c0e245ad964eb8b424ead8d02420c05
Shishir Gururaj <65076777+ShishirGururaj@users.noreply.github.com>
2023-03-19 18:32:58 -0700

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,1 +1,12 @@
+<?php
+
+$dbServerName="localhost";
+$dbUserName="root";
+$dbPassword="";
+$dbName="food";
+
+
+$conn  = mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName);
+    
+    
 
