84cce48e7524bddd4ee29994607211d953788215
Shishir Gururaj <65076777+ShishirGururaj@users.noreply.github.com>
2023-03-19 18:59:52 -0700

    Indentation and Typo fix

--- signup.php
+++ signup.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+
     if(isset($_POST['signup'])){    
         include_once '../includes/dbh.inc.php';
         $name=mysqli_real_escape_string($conn,$_POST['user_name']);
@@ -9,10 +10,12 @@
         $sql = "SELECT * FROM users WHERE user_email='$email'";
         $result = mysqli_query($conn, $sql);
         $resultcheck = mysqli_num_rows($result);
+        
         if($resultcheck > 0){
            header("location: ../index.php?signup=emailAlreadyUsed");
             exit(); 
         }
+        
         else{
             $hashpwd = password_hash($pwd,PASSWORD_DEFAULT);
             $sql = "INSERT INTO users(user_name,user_email,user_password) VALUES ('$name','$email','$hashpwd');";
@@ -22,6 +25,7 @@
             header("location:../index.php?RegisteredSuccessfully");
             exit();
         }
+        
     }else{
         header("location: ../index.php");
         exit();
